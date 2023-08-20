453ec35073c98739a33fa125a61939e3e82e70a4
Chukwudifu Uzoma Okoroafor <31752903+engr-okoroafor@users.noreply.github.com>
2023-08-20 11:29:44 +0100

    Update sketch.js
    Included more functionalities

--- sketch.js
+++ sketch.js
@@ -8,28 +8,37 @@ const inputs = document.querySelectorAll(".input input");
 const inpt1 = inputs[0];
 const inpt2 = inputs[1];
 
-let rates = {};
+let mainContainerRates = {}; // For main container rates
+let rateCardRates = {}; // For rate-card rates
 
-const requestURL = "https://api.exchangerate.host/latest?base=USD";
-"https://api.exchangerate.host/latest?base=EUR";
+const exchangeRatesURLMain = "https://api.exchangerate.host/latest?base=USD";
+const exchangeRatesURLRateCard = "https://api.exchangerate.host/latest?base=NGN"; // Use NGN as the base currency for rate-card
 
+fetchExchangeRates();
 
-fetchRates();
-
-async function fetchRates() {
+async function fetchExchangeRates() {
     try {
-        const response = await fetch(requestURL);
-        const data = await response.json();
-        rates = data.rates;
+        const [responseMain, responseRateCard] = await Promise.all([
+            fetch(exchangeRatesURLMain),
+            fetch(exchangeRatesURLRateCard)
+        ]);
+
+        const dataMain = await responseMain.json();
+        mainContainerRates = dataMain.rates;
+
+        const dataRateCard = await responseRateCard.json();
+        rateCardRates = dataRateCard.rates;
+
         populateOptions();
+        updateRateCard();
     } catch (error) {
-        console.error("Error fetching rates:", error);
+        console.error("Error fetching exchange rates:", error);
     }
 }
 
 function populateOptions() {
     let val = "";
-    Object.keys(rates).forEach(code => {
+    Object.keys(mainContainerRates).forEach(code => {
         const str = `<option value="${code}">${code}</option>`;
         val += str;
     });
@@ -37,20 +46,36 @@ function populateOptions() {
     displayRate();
 }
 
-function convert(val, fromCurr, toCurr) {
+function convert(val, fromCurr, toCurr, rates) {
+    if (fromCurr === "NGN") {
+        return (val * rates[toCurr]).toFixed(2); // Convert directly to NGN without division
+    }
     let v = (val / rates[fromCurr]) * rates[toCurr];
-    let v1 = v.toFixed(3);
-    return v1 == 0.0 ? v.toFixed(5) : v1;
+    return v.toFixed(2); // Always round to 2 decimal places
 }
 
 function displayRate() {
     let v1 = sel1.value;
     let v2 = sel2.value;
 
-    let val = convert(1, v1, v2);
+    let val = convert(1, v1, v2, mainContainerRates);
 
     rate1.innerHTML = `1 ${v1} equals`;
     rate2.innerHTML = `${val} ${v2}`;
+}
+
+function updateRateCard() {
+    const rateCardRows = document.querySelectorAll(".rate-card .rate-row");
+
+    rateCardRows.forEach(row => {
+        const currencyCode = row.querySelector("[data-currency-code]").getAttribute("data-currency-code");
+        const rate = rateCardRates[currencyCode];
+
+        // Convert rate to Naira (NGN)
+        const rateInNaira = convert(1, currencyCode, "NGN", rateCardRates);
+
+        row.querySelector("[data-currency-code]").textContent = `₦ ${rateInNaira}`;
+    });
 }
 
 resultBtn.addEventListener("click", () => {
@@ -61,7 +86,7 @@ resultBtn.addEventListener("click", () => {
     if (isNaN(fromVal)) {
         alert("Enter a Number");
     } else {
-        let cVal = convert(fromVal, fromCurr, toCurr);
+        let cVal = convert(fromVal, fromCurr, toCurr, mainContainerRates);
         inpt2.value = cVal;
     }
 });
@@ -82,3 +107,16 @@ document.querySelector(".swap").addEventListener("click", () => {
 
     displayRate();
 });
+
+// Live Clock
+const clock = document.querySelector(".clock");
+
+function updateClock() {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
+    const formattedDate = now.toLocaleDateString('en-US', options);
+    clock.textContent = formattedDate;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
